c296c533424d536e9b90f876e845769a71dc2852
Ari Porad <git@ariporad.com>
2015-08-23 09:07:10 -0700

    feat: Inital functionality
    Runs script, and responds to the exit code.
    
    Fixes semantic-release/semantic-release#69

--- src/index.js
+++ src/index.js
@@ -1,5 +1,38 @@
 const SRError = require('@semantic-release/error')
+const { exec } = require('child_process')
+const { resolve } = require('path')
+const { stat } = require('fs')
 
-module.exports = function (pluginConfig, config, cb) {
-  cb(null)
+module.exports = function ({ script }, config, cb) {
+  if (!script) return cb(new SRError('No script provided', 'ENOSCRIPT'))
+
+  // If script is the path to a script, run it. Otherwise, it's a command.
+  const scriptPath = resolve(script)
+  stat(scriptPath, (err, stat) => {
+    if (err) return cb(err)
+
+    let command = script
+    if (stat.isFile()) command = scriptPath
+
+    exec(command, (err, stdout, stderr) => {
+      let scriptOutput = ''
+      if (String(stdout)) {
+        scriptOutput += '\n\n'
+        scriptOutput += String(stdout)
+      }
+      if (String(stderr)) {
+        scriptOutput += '\n\n'
+        scriptOutput += String(stderr)
+      }
+
+      if (err) {
+        return cb(new SRError(
+          `Script Exited with non-zero status: ${scriptOutput}`,
+          `ESCRIPTEXIT${err.code}`
+        ))
+      }
+
+      return cb(null)
+    })
+  })
 }
--- test/specs/index.js
+++ test/specs/index.js
@@ -1,12 +1,102 @@
 const proxyquire = require('proxyquire')
 const { test } = require('tap')
-const SRError = require('@semantic-release/error')
+
+const child_processes = []
+const resolves = []
+const stats = []
+
+function makeStatObject (isFile = true) {
+  isFile = !!isFile
+  return {
+    isFile: () => isFile,
+    isDirectory: () => !isFile
+  }
+}
 
 const condition = proxyquire('../../', {
-  // ...
+  child_process: {
+    exec: (cmd, cb) => child_processes.shift()(cmd, cb)
+  },
+  path: {
+    resolve: (p) => resolves.shift()(p)
+  },
+  fs: {
+    stat: (p) => stats.shift()
+  }
 })
 
 test('run-script', (t) => {
-  t.ok(condition)
-  t.end()
+  t.test('calls the plugin', (tt) => {
+    let cpCalled = 0
+    child_processes.push((cmd, cb) => {
+      cpCalled++
+      cb(null, '', '')
+    })
+
+    stats.push(() => makeStatObject(false))
+    resolves.push(() => '/doesnt/exist')
+
+    condition({
+      script: 'rm -rf /'
+    }, {}, (err) => {
+      tt.error(err)
+      tt.is(cpCalled, 1)
+    })
+  })
+
+  t.test('doesn\'t convert commands to paths', (tt) => {
+    child_processes.push((cmd, cb) => {
+      tt.is(cmd, 'rm -rf /')
+      cb(null, '', '')
+    })
+
+    stats.push(() => makeStatObject(false))
+    resolves.push(() => '/doesnt/exist')
+
+    condition({
+      script: 'rm -rf /'
+    }, {}, (err) => {
+      tt.error(err)
+    })
+  })
+
+  t.test('converts paths to absolute paths', (tt) => {
+    const fakeDirname = '/semantic-release/condtion-run-script/test'
+    child_processes.push((cmd, cb) => {
+      tt.is(fakeDirname + '/' + 'test.sh')
+      cb(null, '', '')
+    })
+
+    stats.push(() => makeStatObject(true))
+    resolves.push((p) => fakeDirname + '/' + p)
+
+    condition({
+      script: 'test.sh'
+    }, {}, (err) => {
+      tt.error(err)
+    })
+  })
+
+  t.test('includes the output of the failed script and the exit code', (tt) => {
+    const stdout = 'Blah blah blah'
+    const stderr = 'everything is broken'
+    const errMsg = 'something bad happened'
+    const errCode = 123
+    child_processes.push((cmd, cb) => {
+      const err = new Error(errMsg)
+      err.code = errCode
+      cb(err, stdout, stderr)
+    })
+
+    stats.push(() => makeStatObject(true))
+    resolves.push((p) => '/semantic-release/testing/' + p)
+
+    condition({
+      script: 'test.sh'
+    }, {}, (err) => {
+      tt.isNot(err.code.indexOf(errCode), -1)
+      tt.isNot(err.message.indexOf(stdout), -1)
+      tt.isNot(err.message.indexOf(stderr), -1)
+    })
+  })
 })
